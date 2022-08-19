81fb58d91866a26c9fede5f448c7d99b22f41989
CPM <100002909+criptopromanagement@users.noreply.github.com>
2022-08-19 14:38:41 -0300

    Feature/fe 22 my account (#22)
    * Mi cuenta / Mis datos
    
    * Mis Datos sin api
    
    * Login completo
    
    * Fix Autentication
    
    * Finalizado Mi Cuenta / Mis datos
    
    * Menu
    
    * tabs my account
    
    * RootState bugfix
    
    * Fix theme type
    
    * Error modal
    
    * Fix useSelector
    
    * Fix AlertColor
    
    * Fix null user
    
    Co-authored-by: Kevin Jimenez <kevinricardojs@gmail.com>

--- src/components/account/MyAccountNotification.tsx
+++ src/components/account/MyAccountNotification.tsx
@@ -1,8 +1,8 @@
 import { Alert, IconButton } from "@mui/material";
 import CancelIcon from "@mui/icons-material/Cancel";
 import { closeNotification } from "src/slices/my-account-notificacion-slice";
-import { useDispatch, useSelector, RootStateOrAny } from "react-redux";
 import { FC } from "react";
+import { useDispatch, useSelector } from "src/store";
 
 interface Props {
   showSuccess?: boolean;
