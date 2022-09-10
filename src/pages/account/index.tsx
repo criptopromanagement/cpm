7a8ec86a0f41179f4437769d8bcd2ed08ef99a35
Kevin Jimenez <kevinricardojs@gmail.com>
2022-09-10 08:41:30 -0600

    Fix

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -2,7 +2,7 @@ import { Box, Container, Tab, Tabs, Typography, Divider } from "@mui/material";
 import { useEffect, useState } from "react";
 import { TabMyInfo } from "../../components/account/MyData";
 import { AuthGuard } from "src/components/authentication/auth-guard";
-import { TabSecurity } from "../../components/account/security";
+import { TabSecurity } from "../../components/account/Security";
 import { MyAccountNotification } from "src/components/account/MyAccountNotification";
 import type { NextPage } from "next";
 import { useSelector } from "src/store";
