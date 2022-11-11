1ce41cb05f2d2c125a2b446672ca74c2f5a19fb7
CPM <100002909+criptopromanagement@users.noreply.github.com>
2022-11-11 04:41:44 -0600

    Feature/fe 44 fonts (#46)
    * FE-41 indices, composition and terms
    
    * Move imports from mui/system to mui/material
    
    * performance and terms
    
    * feature/FE-44 Fonts

--- src/theme/base-theme-options.ts
+++ src/theme/base-theme-options.ts
@@ -256,24 +256,24 @@ export const baseThemeOptions: ThemeOptions = {
     },
     fontFamily: 'Poppins',
     body1: {
-      fontSize: '1rem',
+      fontSize: '0.8125rem',
       fontWeight: 400,
-      lineHeight: 1.5
+      lineHeight: 1.2188
     },
     body2: {
-      fontSize: '0.875rem',
-      fontWeight: 400,
-      lineHeight: 1.57
+      fontSize: '0.625rem',
+      fontWeight: 300,
+      lineHeight: 0.9375
     },
     subtitle1: {
-      fontSize: '1rem',
-      fontWeight: 500,
-      lineHeight: 1.75
+      fontSize: '0.8125rem',
+      fontWeight: 400,
+      lineHeight: 1.2188
     },
     subtitle2: {
-      fontSize: '0.875rem',
-      fontWeight: 500,
-      lineHeight: 1.57
+      fontSize: '0.625rem',
+      fontWeight: 300,
+      lineHeight: 0.9375
     },
     overline: {
       fontSize: '0.75rem',
@@ -289,33 +289,24 @@ export const baseThemeOptions: ThemeOptions = {
     },
     h1: {
       fontWeight: 700,
-      fontSize: '3.5rem',
-      lineHeight: 1.375
+      fontSize: '1.6875rem',
+      lineHeight: 1.875
     },
     h2: {
       fontWeight: 700,
-      fontSize: '3rem',
-      lineHeight: 1.375
+      fontSize: '1.125rem',
+      letterSpacing: '0.8px',
+      lineHeight: 1.688
     },
     h3: {
-      fontWeight: 700,
-      fontSize: '2.25rem',
-      lineHeight: 1.375
+      fontWeight: 600,
+      fontSize: '1rem',
+      lineHeight: 1.5
     },
     h4: {
-      fontWeight: 700,
-      fontSize: '2rem',
-      lineHeight: 1.375
-    },
-    h5: {
-      fontWeight: 600,
-      fontSize: '1.5rem',
-      lineHeight: 1.375
-    },
-    h6: {
-      fontWeight: 600,
-      fontSize: '1.125rem',
-      lineHeight: 1.375
+      fontWeight: 500,
+      fontSize: '0.9375rem',
+      lineHeight: 1.125
     }
   },
   zIndex: {
