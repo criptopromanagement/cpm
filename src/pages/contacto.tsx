28aff7f801b45bf8608977c3f6c3270c4313ff6e
Francisco <franciscocalderon1990@gmail.com>
2022-12-06 19:52:24 -0300

    POST de enviar mensaje

--- src/pages/contacto.tsx
+++ src/pages/contacto.tsx
@@ -5,14 +5,17 @@ import Head from 'next/head';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { Footer } from 'src/components/footer';
-import { Box, Button, Grid, Container, Typography, TextField } from '@mui/material';
+import { Box, Button, Grid, Container, Typography, TextField, Alert, IconButton, Collapse } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 import { useState } from 'react';
-
+import apiClientWithoutToken from 'src/services/api-client-without-token';
 
 const Contacto: NextPage = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
+    const [openSucces, setOpenSucces] = useState(false);
+    const [openError, setOpenError] = useState(false)
+    const [loading, setLoading] = useState(false)
 
-    //Defino el esquema de validacion de datos para pasar por hook de useFormik
     const validationSchema = Yup.object({
         name: Yup
             .string()
@@ -34,9 +37,21 @@ const Contacto: NextPage = () => {
             message: ''
         },
         validationSchema: validationSchema,
-        onSubmit: (values) => {
-            // Lo que devuelva la funcion de onSubmit tiene que  llamar a la api para mandar un mail con los campos 
-            console.log(JSON.stringify(values))
+
+        onSubmit: async (values) => {
+            try {
+                setLoading(true)
+                const sendMessage = await apiClientWithoutToken.post("/contactForm", (JSON.stringify(values, null, 2)))
+                if (sendMessage.status === 201) {
+                    setLoading(false)
+                    setOpenSucces(true)
+                } else {
+                    setLoading(false)
+                    setOpenError(true)
+                }
+            } catch (error) {
+                console.log("ERROR=>", error)
+            }
         },
     });
 
@@ -47,6 +62,7 @@ const Contacto: NextPage = () => {
                     CPM | Contacto
                 </title>
             </Head>
+
             <Box
                 component="main"
                 sx={{
@@ -82,6 +98,98 @@ const Contacto: NextPage = () => {
                             onClose={(): void => setIsSidebarOpen(false)}
                             open={isSidebarOpen}
                         />
+
+                        {/* Alert de mensaje Enviado */}
+                        {loading ? "Enviando..." :
+                            <Box
+                                sx={{
+                                    width: '100%',
+                                    p: 1
+                                }}>
+                                <Collapse
+                                    in={openSucces}>
+
+                                    <Alert
+                                        severity="success"
+                                        action={
+                                            <IconButton
+                                                aria-label="close"
+                                                color="inherit"
+                                                onClick={() => {
+                                                    setOpenSucces(false);
+                                                }}
+                                            >
+                                                <CloseIcon
+                                                    style={{
+                                                        backgroundColor: "white",
+                                                        borderRadius: "50%",
+                                                        height: "100%",
+                                                        width: "30px",
+                                                    }}
+                                                />
+                                            </IconButton>
+                                        }
+                                        sx={{
+                                            mb: 2,
+                                        }}
+                                    >
+                                        <Typography
+                                            color="white"
+                                        >
+                                            Gracias por envíar tu mensaje.
+                                            <br />
+                                            Te responderemos lo más pronto posible.
+                                        </Typography>
+                                    </Alert>
+
+                                </Collapse>
+                            </Box>
+                        }
+
+                        {/* Alert de ERROR al enviar el mensaje */}
+                        {loading ? "" :
+                            <Box
+                                sx={{
+                                    width: '100%',
+                                    p: 1
+                                }}>
+                                <Collapse
+                                    in={openError}>
+                                    <Alert
+                                        severity="error"
+                                        action={
+                                            <IconButton
+                                                aria-label="close"
+                                                color="inherit"
+                                                onClick={() => {
+                                                    setOpenError(false);
+                                                }}
+                                            >
+                                                <CloseIcon
+                                                    style={{
+                                                        backgroundColor: "white",
+                                                        borderRadius: "50%",
+                                                        height: "100%",
+                                                        width: "30px",
+                                                    }}
+                                                />
+                                            </IconButton>
+                                        }
+                                        sx={{
+                                            mb: 2,
+                                        }}
+                                    >
+                                        <Typography
+                                            color="white"
+                                        >
+                                            Oops! Algo salio mal.
+                                            <br />
+                                            Por favor intenta mas tarde.
+                                        </Typography>
+                                    </Alert>
+                                </Collapse>
+                            </Box>
+                        }
                         <form
                             onSubmit={formik.handleSubmit}
                             noValidate
@@ -101,7 +209,6 @@ const Contacto: NextPage = () => {
                                 variant="outlined"
                                 margin='normal'
                                 fullWidth
-
                             />
                             <TextField
                                 onChange={formik.handleChange}
@@ -114,7 +221,6 @@ const Contacto: NextPage = () => {
                                 variant="outlined"
                                 margin='normal'
                                 fullWidth
-
                             />
                             <TextField
                                 onChange={formik.handleChange}
@@ -129,7 +235,6 @@ const Contacto: NextPage = () => {
                                 multiline
                                 rows={6}
                                 fullWidth
-
                             />
                             <Button
                                 style={{
@@ -139,6 +244,7 @@ const Contacto: NextPage = () => {
                                 variant="contained"
                                 fullWidth
                                 type="submit"
+                                disabled={formik.isSubmitting}
                             >
                                 Enviar
                             </Button>
@@ -153,7 +259,6 @@ const Contacto: NextPage = () => {
                         top: -80,
                         zIndex: -1,
                         transform: "rotateY(180deg)",
-
                     }}
                 >
                 </Box>
