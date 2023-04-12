b39d84d837e12f535bd62a07c635efa49cefd81d
Kevin Jimenez <kevinricardojs@gmail.com>
2023-04-12 09:08:28 -0600

    mobile slice (#60)
    (cherry picked from commit d5791899d7452de310aec0fb8c5116723e71382c)
    
    Co-authored-by: Kevin Jimenez <krjimenez@gyt.com.gt>

--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -4,6 +4,7 @@ import { reducer as myAccountNotificationReducer } from "../slices/my-account-no
 import { reducer as logoutModalReducer } from "../slices/logout-modal-slice";
 import { reducer as notificationReducer } from "../slices/notification-slice";
 import { reducer as transactionsReducer } from "../slices/transactions-slice";
+import { reducer as isMobileReducer } from "../slices/is-mobile-slice";
 
 export const rootReducer = combineReducers({
   user: userReducer,
@@ -11,4 +12,5 @@ export const rootReducer = combineReducers({
   logoutModal: logoutModalReducer,
   notification: notificationReducer,
   transactions: transactionsReducer,
+  isMobile: isMobileReducer
 });
