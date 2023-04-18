8243ad49e04e17a02419696a440263df16a57ab6
criptopromanagement <jgomez@cpmanager.io>
2023-04-18 21:45:54 +0200

    Add performance chart

--- src/components/home/home-card-plan.tsx
+++ src/components/home/home-card-plan.tsx
@@ -3,11 +3,10 @@ import {
   Card,
   CardMedia,
   Grid,
-  Link,
   Stack,
   Typography,
 } from "@mui/material";
-import { Box, margin } from "@mui/system";
+import { Box } from "@mui/system";
 import { styled } from "@mui/material/styles";
 import { Plan } from "src/types/plan";
 import NextLink from "next/link";
@@ -28,7 +27,6 @@ export const CardPlan: FC<Props> = ({ plan }: Props) => {
   return (
     <div>
       {isMobile ? (
-
         <Grid
           item
           md={4}
@@ -78,16 +76,16 @@ export const CardPlan: FC<Props> = ({ plan }: Props) => {
               >
                 {plan.subdescription}
               </Typography>
-              </Box>
-              <Box sx={{height:'100%'}}>
-                <Typography 
-                  variant="h5"
-                  sx={{ mt: 3, mb: 2 }}
-                >
-                  Rendimiento
-                </Typography>
-                {/* <Performance /> */}
-              </Box>
+            </Box>
+            <Box sx={{ height: '100%' }}>
+              <Typography
+                variant="h5"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Rendimiento
+              </Typography>
+              <Performance />
+            </Box>
             <Stack
               direction={{ xs: 'column', sm: 'row' }}
               spacing={2}
@@ -126,106 +124,103 @@ export const CardPlan: FC<Props> = ({ plan }: Props) => {
         </Grid>
       ) :
         (
-          <Box sx={{flexDirection: "column", border: "2px solid white", borderRadius: "16px"}}>
-
-          <Grid container width={{ md: "800px", lg: "1000px", xl: "1400px"}}
-          > 
-          <Grid
-            item
-            md={6}
-            xs={12}
-          >
-              <BlogPostCardMediaWrapper>
-                <CardMedia
-                  image={plan.cover}
-                  sx={{
-                    height: "100%",
-                    position: "absolute",
-                    top: 0,
-                    width: "100%",
-                    borderRadius: "16px 0 0 16px",
-                  }}
-                />
-              </BlogPostCardMediaWrapper>
-          </Grid>
-          <Grid item md={6} xs={12}>
-              <Box sx={{ mt: 3, ml: 3, mr: 3 }}>
-                <Typography variant="h5">{plan.title}</Typography>
-                <Typography
-                  color="textSecondary"
-                  sx={{
-                    mt: 1,
-                    overflow: "hidden",
-                    textOverflow: "ellipsis",
-                    WebkitBoxOrient: "vertical",
-                    WebkitLineClamp: 2,
-                  }}
-                  variant="body1"
-                >
-                  {plan.description}
-                </Typography>
-                <Typography
-                  color="textSecondary"
-                  sx={{
-                    overflow: "hidden",
-                    textOverflow: "ellipsis",
-                    WebkitBoxOrient: "vertical",
-                    WebkitLineClamp: 2,
-                  }}
-                  variant="body1"
-                >
-                  {plan.subdescription}
-                </Typography>
-              </Box>
-              <Typography 
-                variant="h5"
-                sx={{ mt: 3, mb: 2, ml: 3 }}
+          <Box sx={{ flexDirection: "column", border: "2px solid white", borderRadius: "16px" }}>
+            <Grid container width={{ md: "800px", lg: "1000px", xl: "1400px" }}
+            >
+              <Grid
+                item
+                md={6}
+                xs={12}
               >
-                Rendimiento
-              </Typography>
-              {/* <Performance /> */}
-              <Stack
-                direction={{ xs: 'column', sm: 'row' }}
-                spacing={2}
-                pt={2}
-                ml={3}
-                mr={3}
-                justifyContent="space-between"
-                alignItems="stretch"
-              >
-                <NextLink
-                  href={plan.informationLink}
-                  passHref
-                >
-                  <Button
-                    component="a"
-                    size="large"
-                    fullWidth
-                    variant="outlined"
+                <BlogPostCardMediaWrapper>
+                  <CardMedia
+                    image={plan.cover}
+                    sx={{
+                      height: "100%",
+                      position: "absolute",
+                      top: 0,
+                      width: "100%",
+                      borderRadius: "16px 0 0 16px",
+                    }}
+                  />
+                </BlogPostCardMediaWrapper>
+              </Grid>
+              <Grid item md={6} xs={12}>
+                <Box sx={{ mt: 3, ml: 3, mr: 3 }}>
+                  <Typography variant="h5">{plan.title}</Typography>
+                  <Typography
+                    color="textSecondary"
+                    sx={{
+                      mt: 1,
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                      WebkitBoxOrient: "vertical",
+                      WebkitLineClamp: 2,
+                    }}
+                    variant="body1"
                   >
-                    Conocer más
-                  </Button>
-                </NextLink>
-                <NextLink
-                  href={plan.investmentLink}
-                  passHref
-                >
-                  <Button
-                    component="a"
-                    size="large"
-                    fullWidth
-                    variant="contained"
+                    {plan.description}
+                  </Typography>
+                  <Typography
+                    color="textSecondary"
+                    sx={{
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                      WebkitBoxOrient: "vertical",
+                      WebkitLineClamp: 2,
+                    }}
+                    variant="body1"
                   >
-                    Invertir
-                  </Button>
-                </NextLink>
-              </Stack>
-          </Grid>
+                    {plan.subdescription}
+                  </Typography>
+                </Box>
+                <Typography
+                  variant="h5"
+                  sx={{ mt: 3, mb: 2, ml: 3 }}
+                >
+                  Rendimiento
+                </Typography>
+                <Performance />
+                <Stack
+                  direction={{ xs: 'column', sm: 'row' }}
+                  spacing={2}
+                  pt={2}
+                  ml={3}
+                  mr={3}
+                  justifyContent="space-between"
+                  alignItems="stretch"
+                >
+                  <NextLink
+                    href={plan.informationLink}
+                    passHref
+                  >
+                    <Button
+                      component="a"
+                      size="large"
+                      fullWidth
+                      variant="outlined"
+                    >
+                      Conocer más
+                    </Button>
+                  </NextLink>
+                  <NextLink
+                    href={plan.investmentLink}
+                    passHref
+                  >
+                    <Button
+                      component="a"
+                      size="large"
+                      fullWidth
+                      variant="contained"
+                    >
+                      Invertir
+                    </Button>
+                  </NextLink>
+                </Stack>
+              </Grid>
             </Grid>
           </Box>
-
         )}
     </div>
-
   )
 };
