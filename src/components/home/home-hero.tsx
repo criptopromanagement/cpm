428d09c2741804fe4949cd0ed9f7b7af720754e3
CPM <100002909+criptopromanagement@users.noreply.github.com>
2023-04-19 09:39:53 -0600

    Feature/fe 51 vista desktop home plans (#63)
    * use isMobile from redux
    
    * Vista desktop home plans
    
    * Add performance chart

--- src/components/home/home-hero.tsx
+++ src/components/home/home-hero.tsx
@@ -1,9 +1,12 @@
 import type { FC } from "react";
 import NextLink from "next/link";
 import { Box, Button, Container, Typography, useMediaQuery, Grid } from "@mui/material";
+import { useSelector } from 'react-redux';
+import { RootState } from 'src/store';
+
 export const HomeHero: FC = (props) => {
 
-  const isMobile = useMediaQuery('(max-width:600px)');
+const isMobile = useSelector((state:RootState) => state.isMobile.isMobile);
 
   return (
     <>
@@ -21,7 +24,7 @@ export const HomeHero: FC = (props) => {
                 maxWidth: 450,
                 width: "100%",
                 mx: "auto",
-                backgroundColor: 'primary.nav', 
+                backgroundColor: 'primary.nav',
               }}
             >
               <Box
@@ -90,100 +93,94 @@ export const HomeHero: FC = (props) => {
 
         ) : (
           // desktop
-          <div className="hero" style={{padding: 100}}>
-          <Grid item
-            xs={12}
-            sm={12}
-            md={6}
-            lg={6}>
-            <Container
-              maxWidth="md"
-              sx={{
-                alignItems: "left",
-                display: "flex",
-                flexDirection: "column",
-                padding: 5,
-              }}
-            >
-              <Typography
-                align="left"
-                variant="h1"
+          <div style={{ padding: 100 }}>
+            <Grid item
+              xs={12}
+              sm={12}
+              md={6}
+              lg={6}>
+              <Container
+                maxWidth="md"
+                sx={{
+                  alignItems: "left",
+                  display: "flex",
+                  flexDirection: "column",
+                  padding: 5,
+                }}
               >
-                Fondos cripto
-              </Typography>
-              <Typography
-                align="left"
-                variant="h1"
-              >
-                indexados
-              </Typography>
-              <Typography
-                align="left"
-                variant="subtitle2"
-                sx={{ py: 1 }}
-              >
-                Estrategia automatizada
-              </Typography>
-              <Typography
-                align="left"
-                variant="subtitle2"
-                sx={{ py: 1 }}
-              >
-                Exposición diversificada
-              </Typography>
+                <Typography
+                  align="left"
+                  variant="h1"
+                >
+                  Fondos cripto
+                </Typography>
+                <Typography
+                  align="left"
+                  variant="h1"
+                >
+                  indexados
+                </Typography>
+                <Typography
+                  align="left"
+                  variant="subtitle2"
+                  sx={{ py: 1 }}
+                >
+                  Estrategia automatizada
+                </Typography>
+                <Typography
+                  align="left"
+                  variant="subtitle2"
+                  sx={{ py: 1 }}
+                >
+                  Exposición diversificada
+                </Typography>
               </Container>
 
 
               <Container
-              maxWidth="md"
-              sx={{
-                alignItems: "right",
-                display: "flex",
-                flexDirection: "column",
-              }}
-            >
-              <NextLink
-                href="/dashboard"
-                passHref
-              >
-                <Button
-                  sx={{ maxWidth: "150px" }}
-                  component="a"
-                  size="large"
-                  fullWidth
-                  variant="contained"
-                >
-                  Invertir
-                </Button>
-              </NextLink>
-              <Box
+                maxWidth="md"
                 sx={{
-                  justifyContent: "flex-end",
-                  pt: "calc(400 / 450 * 100%)",
-                  "& img": {
-                    height: "auto",
-                    position: "absolute",
-                    top: 50,
-                    left: 550,
-                    right: 250
-                  },
+                  alignItems: "right",
+                  display: "flex",
+                  flexDirection: "column",
                 }}
               >
-                <img
-                  alt="hero"
-                  src="/static/home/hero.svg"
-                />
-              </Box>
-            </Container>
-
-
-
-              
-          </Grid>
+                <NextLink
+                  href="/dashboard"
+                  passHref
+                >
+                  <Button
+                    sx={{ maxWidth: "150px" }}
+                    component="a"
+                    size="large"
+                    fullWidth
+                    variant="contained"
+                  >
+                    Invertir
+                  </Button>
+                </NextLink>
+                <Box
+                  sx={{
+                    justifyContent: "flex-end",
+                    pt: "calc(400 / 450 * 100%)",
+                    "& img": {
+                      height: "auto",
+                      position: "absolute",
+                      top: 50,
+                      left: 550,
+                      right: 250
+                    },
+                  }}
+                >
+                  <img
+                    alt="hero"
+                    src="/static/home/hero.svg"
+                  />
+                </Box>
+              </Container>
+            </Grid>
           </div>
-        )
-
-        }
+        )}
       </div>
     </>
   );
--- src/components/home/home-plans.tsx
+++ src/components/home/home-plans.tsx
@@ -1,41 +1,102 @@
 import type { FC } from "react";
 import { Box } from "@mui/system";
-import { Container, Grid } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 import { Plan } from "src/types/plan";
 import { CardPlan } from "./home-card-plan";
+import { useSelector } from "react-redux";
+import { RootState } from "src/store";
 
 export const HomePlans: FC = (props) => {
+  const isMobile = useSelector((state: RootState) => state.isMobile.isMobile);
+
   const plans: Plan[] = [
     {
       id: "1",
       cover: "/static/home/foundation.svg",
       title: "Foundation",
       description:
-        "Obtené una exposición diversificada en los principales proyectos de plataformas de contratos inteligentes.",
-      subdescription: "Foundation consta de un índice con las cripto mas valiosas y mejor consolidadas en el sector según la capitalización de mercado",
+        "Índice que reúne a las 10 plataformas de contratos inteligentes mas importantes y mejor consolidadas del sector.",
+      subdescription: "Ponderado según la capitalización de mercado y rebalanceado mensualmente.",
       informationLink: "",
       investmentLink: "",
     },
   ];
   return (
-    <Box
-      component="main"
-      sx={{
-        flexGrow: 5,
-      }}
-    >
-      <Container maxWidth="lg">
-        <Grid
-          container
-          justifyContent="center"
-          spacing={5}
-          padding={2}
+    isMobile ? (
+      <>
+      <Box marginLeft={2}>
+        <Typography align="left"
+          variant="h1">
+          Fondos de
+          inversión cripto
+        </Typography>
+        <Typography
+          align="left"
+          variant="subtitle2"
+          sx={{ py: 1 }}
         >
-          {plans.map((plan) => (
-            <CardPlan key={plan.id} plan={plan} />
-          ))}
-        </Grid>
-      </Container>
-    </Box>
+          Basados en Índices con los principales criptoactivos por sector, rebalanceado períodicamente
+        </Typography>
+        </Box>
+        <Box
+          component="main"
+          marginTop={5}
+          marginLeft={5}
+          sx={{
+            flexGrow: 5,
+          }}
+        >
+          <Container maxWidth="lg">
+            <Grid
+              container
+              justifyContent="center"
+              spacing={5}
+              padding={2}
+            >
+              {plans.map((plan) => (
+                <CardPlan key={plan.id} plan={plan} />
+              ))}
+            </Grid>
+          </Container>
+        </Box>
+      </>
+    ) : (
+      <>
+        <Box marginLeft={'100px'} marginBottom={5}>
+          <Typography align="left"
+            variant="h1">
+            Fondos de
+            inversión cripto
+          </Typography>
+          <Typography
+            align="left"
+            variant="subtitle2"
+            sx={{ py: 1 }}
+          >
+            Basados en Índices con los principales criptoactivos por sector, rebalanceado períodicamente
+          </Typography>
+          </Box>
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 5,
+            flexDirection: "column",
+          }}
+        >
+          <Container maxWidth="lg">
+            <Grid
+              container
+              justifyContent="center"
+              spacing={5}
+              padding={2}
+            >
+              {plans.map((plan) => (
+                <CardPlan key={plan.id} plan={plan} />
+              ))}
+            </Grid>
+          </Container>
+        </Box>
+      </>
+    )
   );
 };
