65240bc5e662efc8fcb395329a68fa1d724d41ea
Francisco Calderon <franciscocalderon1990@gmail.com>
2023-05-16 21:02:49 -0300

    Vista desktop seccion indices

--- src/services/api-client-without-token.ts
+++ src/services/api-client-without-token.ts
@@ -5,6 +5,9 @@ axios.defaults.baseURL = "https://cpm-api-vejr.onrender.com/api"
 const apiClientWithoutToken = {
     post: (url: string, body: string) => {
         return axios.post(url, body)
+    },
+    get: (url: string) => {
+        return axios.get(url)
     }
 };
 
