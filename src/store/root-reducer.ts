a039213cd5803ca7ec6062602878fc83a4f4a167
CPM <100002909+criptopromanagement@users.noreply.github.com>
2023-05-23 13:21:33 -0600

    Revert "Feature/c10 18 grafico de performance utilizar datos de la api y ajustar el diseño al figma actual (#73)" (#74)
    This reverts commit a09795dff8b97122be099c5098a768d0c320f691.

--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -5,7 +5,6 @@ import { reducer as logoutModalReducer } from "../slices/logout-modal-slice";
 import { reducer as notificationReducer } from "../slices/notification-slice";
 import { reducer as transactionsReducer } from "../slices/transactions-slice";
 import { reducer as mobileReducer } from "../slices/mobile-slice";
-import { reducer as performanceReducer } from "../slices/performance-slice";
 
 export const rootReducer = combineReducers({
   user: userReducer,
@@ -13,6 +12,5 @@ export const rootReducer = combineReducers({
   logoutModal: logoutModalReducer,
   notification: notificationReducer,
   transactions: transactionsReducer,
-  mobile: mobileReducer,
-  performance: performanceReducer
+  mobile: mobileReducer
 });
