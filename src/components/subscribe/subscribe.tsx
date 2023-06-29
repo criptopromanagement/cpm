2549b91317d409215898d1b71c519ebca3bfb8ac
CPM <jgomez@cpmanager.io>
2023-06-29 16:42:18 +0200

    C10-29 suscripcion mobile

--- src/components/subscribe/subscribe.tsx
+++ src/components/subscribe/subscribe.tsx
@@ -1,19 +1,19 @@
 import { Container, Typography, TextField, Button, Box } from '@mui/material'
-import { flexbox } from '@mui/system'
 import React from 'react'
 
 const Subscribe = () => {
+    
   return (
-    <Container sx={{margin:'10rem'}} >
+    <Container>
       <Typography variant='h3' align='center'>
       Suscribite y recibí información sobre las novedades de la industria cripto
       </Typography>
-      <Box display='flex' justifyContent='center' alignItems='center' marginTop= '2rem' paddingX= '20rem'>
-      <TextField id="outlined-basic" label="Tu email" variant="outlined" fullWidth sx={{marginRight: '1rem'}} />
+      <Box display='flex' justifyContent='center' alignItems='center' marginTop= '2rem' marginBottom={{xs: '2rem'}} paddingX= '2rem' flexDirection= {{ xs: 'column' , md: 'row'}}>
+      <TextField id="outlined-basic" label="Tu email" variant="outlined" fullWidth multiline rows={0.5} sx={{marginRight: '1rem'}} />
       <Button
                   component="a"
-                  size="large"
                   variant="contained"
+                  sx= {{marginTop: { xs: '1rem', md: 0} }}
                 >
                   Suscribirme
                 </Button>
