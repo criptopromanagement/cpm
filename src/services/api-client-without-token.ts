30d290f29de44543b73917d2ba1a8ad8846a0f63
CPM <100002909+criptopromanagement@users.noreply.github.com>
2023-06-29 17:59:15 +0200

    new service (#83)

--- src/services/api-client-without-token.ts
+++ src/services/api-client-without-token.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://cpm-api-vejr.onrender.com/api"
+axios.defaults.baseURL = "https://cpm-tst.onrender.com/api"
 
 const apiClientWithoutToken = {
     post: (url: string, body: string) => {
