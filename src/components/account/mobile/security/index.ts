f364e5f5c7d2e0aa22b878dcc442cd6c50c04df6
AcoJuli <acojuli@gmail.com>
2023-12-05 15:51:26 +0100

    reset pass POC

--- src/components/account/mobile/security/index.ts
+++ src/components/account/mobile/security/index.ts
@@ -1,4 +1,4 @@
 export { TabSecurity } from "./tab-security";
-export { FormChangePassword } from "./form-change-password";
+export { FormResetPassword } from "./form-reset-password";
 export { LoginHistory } from "./login-history";
 export { AccountLogout } from "./account-logout";
