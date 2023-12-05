6191878d0e38513ead5ebbe00b4529b7cc96c0af
AcoJuli <acojuli@gmail.com>
2023-12-05 19:25:28 +0100

    POC reset pass

--- src/components/account/desktop/index.tsx
+++ src/components/account/desktop/index.tsx
@@ -1,12 +1,12 @@
 import { Grid, Typography } from "@mui/material";
 import React from "react";
-import { FormChangePassword, LoginHistory } from "../mobile/security";
+import { FormResetPassword, LoginHistory } from "../mobile/security";
 import { MultiFactorAuth } from "../mobile/security/multi-factor-auth";
 
 export const DesktopSecurity = () => {
   return (
     <Grid container item md={12} lg={12} spacing={2}>
-      <FormChangePassword />
+      <FormResetPassword />
       <MultiFactorAuth />
       <LoginHistory />
     </Grid>
