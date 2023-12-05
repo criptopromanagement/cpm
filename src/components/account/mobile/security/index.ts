68a70afa41388ab987ddaa1641e1f4a3d34b4364
CPM <100002909+criptopromanagement@users.noreply.github.com>
2023-12-05 16:11:55 +0100

    reset pass POC (#103)
    * reset pass POC
    
    * Restore FormChange export
    
    ---------
    
    Co-authored-by: AcoJuli <acojuli@gmail.com>

--- src/components/account/mobile/security/index.ts
+++ src/components/account/mobile/security/index.ts
@@ -1,4 +1,5 @@
 export { TabSecurity } from "./tab-security";
+export { FormResetPassword } from "./form-reset-password";
 export { FormChangePassword } from "./form-change-password";
 export { LoginHistory } from "./login-history";
 export { AccountLogout } from "./account-logout";
